37e998dd446d52b5eb38e0202a18a8c7aa0567e7
Libor Polčák <ipolcak@fit.vutbr.cz>
2017-04-21 17:44:09 +0200

    Do not duplicate sed commands
    Provide generic functions fix_problem_beginning and fix_problem_end.

--- wave.sh
+++ wave.sh
@@ -15,19 +15,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function fix_file() {
-	# Squeeze cite and ref at the beginning of a line to the previous line
-	# See also https://stackoverflow.com/questions/12129870/how-can-i-remove-a-line-feed-newline-before-a-pattern-using-sed
-	sed -i ':r;$!{N;br};s/\n\\cite/~\\cite/g' $1
-	sed -i ':r;$!{N;br};s/\n\\ref/~\\ref/g' $1
-	# Squeeze (number)\nsqueeze on the previous line
-	sed -i ':r;$!{N;br};s/\(([0-9]+)\)\n/\1~/g' $1
+function fix_problem_beginning {
+	FIND=$1
+	FILE=$2
 
-	# Replace spaces before cite and ref
-	sed -i 's/ \\cite/\~\\cite/g' $1
-	sed -i 's/ \\ref/\~\\ref/g' $1
-	# Replace spaces after (num) Something
-	sed -i 's/\(([0-9]+)\) /\1~/g' $1
+	# See also https://stackoverflow.com/questions/12129870/how-can-i-remove-a-line-feed-newline-before-a-pattern-using-sed
+	sed -i ":r;$!{N;br};s/\n${FIND}/~${FIND}/g" "$FILE"
+	sed -i "s/ ${FIND}/~${FIND}/g" "$FILE"
+}
+
+function fix_problem_end {
+	FIND=$1
+	FILE=$2
+
+	# See also https://stackoverflow.com/questions/12129870/how-can-i-remove-a-line-feed-newline-before-a-pattern-using-sed
+	sed -i ":r;$!{N;br};s/\(${FIND}\)\n/\1~/g" "$FILE"
+	sed -i "s/\(${FIND}\) \b/\1~/g" "$FILE"
+}
+
+function fix_file() {
+	# Fix \cite and \ref
+	fix_problem_beginning '\\cite' $1
+	fix_problem_beginning '\\ref' $1
+	# Squeeze (number)\nsqueeze on the previous line
+	fix_problem_end '([0-9]+)' $1
+
 }
 
 while [ $# -gt 0 ];
